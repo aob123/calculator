9cdc81349e8cdf1df26727d6317f787701eca34a
aob123 <alexobeswick@gmail.com>
2020-08-17 21:47:50 +0200

    Calculator design completed with CSS. It also shows clicked digits on the display. Next step is to wire up JS and make it functional.

--- script.js
+++ script.js
@@ -45,3 +45,57 @@ function operate(a, operator, b) {
 }
 
 console.log(operate(7, division, 9));
+
+// Functions to populate display with integers and operators
+
+
+
+/*const btnOne = document.getElementById("btnOne");
+const btnTwo = document.getElementById("btnTwo");
+const btnThree = document.getElementById("btnThree");
+const btnFour = document.getElementById("btnFour");
+const btnFive = document.getElementById("btnFive");
+const btnSix = document.getElementById("btnSix");
+const btnSeven = document.getElementById("btnSeven");
+const btnEight = document.getElementById("btnEight");
+const btnNine = document.getElementById("btnNine");
+const btnZero = document.getElementById("btnZero");
+const btnComma = document.getElementById("btnComma");
+const btnClear = document.getElementById("btnClear");
+const btnAdd = document.getElementById("btnAdd");
+const btnSubtract = document.getElementById("btnSubtract");
+const btnMultiply = document.getElementById("btnMultiply");
+const btnDivide = document.getElementById("btnDivide");
+const btnEquals = document.getElementById("btnEquals");*/
+
+let calcStore = [];
+
+let btnNumbers = document.querySelectorAll(".btnNumbers");
+
+function toDisplay(){
+
+    let display = document.getElementById("displayText");
+
+        for (const button of btnNumbers) {
+            button.addEventListener("click", () => {
+                display.textContent = button.textContent;
+                return calcStore.push(button.textContent);
+                
+            });
+        }
+        
+    
+}
+
+let sum = calcStore.reduce(function(a,b) {
+
+    return a + b;
+
+}, 0);
+
+toDisplay();
+
+
+
+
+
